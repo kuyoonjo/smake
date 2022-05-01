61cc52940d554b49dc8e3a2fc8a21a4e1b91ffec
Yu Chen <yu.chen@live.ie>
2022-05-01 16:19:19 +0800

    v2

--- src/bin.ts
+++ src/bin.ts
@@ -9,6 +9,7 @@ import { Log } from './Log';
 import { build } from './build';
 import { modulesDir } from '.';
 import { readdir } from 'fs/promises';
+import { join } from './join';
 
 (async () => {
   const program = new Command();
@@ -44,26 +45,19 @@ import { readdir } from 'fs/promises';
 
   const md = modulesDir();
   const dirs = await readdir(md);
+  const pluginsDirname = '@smake-plugins';
+  if (dirs.includes(pluginsDirname)) {
+    const pdirs = await readdir(join(md, pluginsDirname));
+    for (const p of pdirs) {
+      const m = require(join(pluginsDirname, p));
+      if (m.command) m.command(program);
+    }
+  }
   const plugins = dirs.filter((d) => d.startsWith('smake-plugin-'));
   for (const p of plugins) {
     const m = require(p);
     if (m.command) m.command(program);
   }
-
-  // program
-  //   .command('ide')
-  //   .option('-f, --file <path>', 'specify the smake build file')
-  //   .action((command) => {
-  //     const opts = command.opts();
-  //     const file = resolve(opts.file || 'smake.js');
-  //     if (!existsSync(file)) {
-  //       Log.e('Cannot find', yellow(file));
-  //       process.exit(1);
-  //     }
-  //     const m = require(file);
-  //     const args = ['ide'];
-  //     run(m, args);
-  //   });
 
   program.on('command:*', (operands: any) => {
     Log.e('unknown command', cyan(operands[0]));
